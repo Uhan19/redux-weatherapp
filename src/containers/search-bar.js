fb5f321499e6e39a069918939f66ed69cef5bc4d
Yuehan Duan <yuehanduan@yahoo.com>
2018-05-05 11:49:02 -0700

    Install redux promise

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -7,15 +7,21 @@ export default class SearchBar extends Component {
       term: "",
     };
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
   handleInputChange(e) {
     this.setState({ term: e.target.value });
   }
 
+  handleFormSubmit(e) {
+    e.preventDefault();
+    // fetchh weather data
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.handleFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forecare in your favorite cities"
           className="form-control"
